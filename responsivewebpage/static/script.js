b203f90ade921902236dedc7871b5dea5545e95a
Sahithi <2200031221cseh@gmail.com>
2024-12-16 15:01:03 +0530

    Initial commit

---
+++ responsivewebpage/static/script.js
@@ -0,0 +1,9 @@
+// Change Navbar Background on Scroll
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
+    } else {
+        navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
+    }
+});
